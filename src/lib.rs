e69a25e6f0d6ce4f18b320632f16ec7e90cf396f
Ubique0305 <1071763478@qq.com>
2019-07-26 17:08:32 +0800

    style(lib.rs): delete blank line
    no

--- src/lib.rs
+++ src/lib.rs
@@ -9,30 +9,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     } else {
         search_case_insensitive(&config.query, &contents)
     };
-
     for line in results {
         println!("{}", line);
     }
-
     Ok(())
 }
 pub struct Config {
     pub query: String,
     pub filename: String,
     pub case_sensitive: bool,
-
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-
         let query = args[1].clone();
         let filename = args[2].clone();
-
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
         Ok(Config { query, filename, case_sensitive })
     }
 }
