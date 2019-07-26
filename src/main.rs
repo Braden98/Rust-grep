f5ebf290b771680d200467b8bd44ad0486159992
Ubique0305 <1071763478@qq.com>
2019-07-26 15:28:29 +0800

    feat:process Result from run(config)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env; 
 use std::fs;
 use std::process;
+use std::error::Error;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,7 +14,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 
 }
 struct Config {
@@ -39,9 +45,12 @@ fn parse_config(args: &[String]) -> Config {
     Config { query, filename }
 }
 
-fn run(config:Config) {
+fn run(config:Config) ->Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("read uncorrectly");
-    println!("content:\n{}",contents );
+    println!("With text:\n{}", contents);
+
+    Ok(())
+
+    
 }
