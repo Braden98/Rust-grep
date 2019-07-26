a4abd874e86ea04577b1d7c8d826c34b54181564
Ubique0305 <1071763478@qq.com>
2019-07-26 15:24:05 +0800

    refactor:add run to wrap logic code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 use std::env; 
 use std::fs;
-
+use std::process;
 fn main() {
-    println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing args :{}",err );
+        process::exit(1);
+    });
    
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("read uncorrectly");
-    println!("content:\n{}",contents );
 }
 struct Config {
     query: String,
     filename: String,
 }
 impl Config {
-    fn new(args:&[String]) -> Config{
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {query,filename}
+        Ok(Config { query, filename })
     }
 }
 
@@ -33,4 +37,11 @@ fn parse_config(args: &[String]) -> Config {
     let filename = args[2].clone();
 
     Config { query, filename }
+}
+
+fn run(config:Config) {
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("read uncorrectly");
+    println!("content:\n{}",contents );
 }
