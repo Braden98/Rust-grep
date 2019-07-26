6ec7300b6499b7f4376c3efa131d894f5d86e6f1
Ubique0305 <1071763478@qq.com>
2019-07-26 14:53:33 +0800

    feat:add buider of Config

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,33 @@ use std::fs;
 fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
-    println!("{:?}",args);
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = Config::new(&args);
+   
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("read uncorrectly");
     println!("content:\n{}",contents );
+}
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args:&[String]) -> Config{
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config {query,filename}
+    }
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
